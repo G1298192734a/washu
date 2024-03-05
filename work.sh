73cc0d6cee132d17f1272636eb86bd8f06b0be32
G1298192734a <shaojie@frasergen.com>
2024-03-05 23:51:20 +0800

    washu

---
+++ work.sh
@@ -0,0 +1,11 @@
+#git config --global user.name G1298192734a
+#git config --global user.mail shaojie@frasergen.com
+#ssh-keygen -t rsa -C shaojie@frasergen.com
+#gedit id_rsa.pub
+
+git init
+git add whfs-xs-230891
+git commit -m washu
+git remote add origin git@github.com:G1298192734a/washu.git
+git push origin master
+
